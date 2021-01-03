c87e5b37a659586977a59568e25ca5eb478007c9
heidenbloed <git@heidenblut.eu>
2021-01-03 20:39:42 +0100

    Update current user at service installation.

--- install_service.sh
+++ install_service.sh
@@ -1,12 +1,17 @@
 echo "Stop service"
 sudo systemctl stop switch_device_router.service
-echo "Install virtual python environment"
-python3.8 -m venv venv
+if [ ! -f venv/bin/activate ]; then
+  echo "Install virtual python environment"
+  python3.8 -m venv venv
+fi
 source venv/bin/activate
 echo "Install python requirements"
 pip install -r ./requirements.txt
 echo "Copy service"
 sudo cp switch_device_router.service /etc/systemd/system/
+echo "Adapt service for current user"
+sudo sed -i "s@USERNAME@$USER@" /etc/systemd/system/switch_device_router.service
+sudo sed -i "s@HOMEDIR@$HOME@" /etc/systemd/system/switch_device_router.service
 echo "Enable service"
 sudo systemctl enable switch_device_router.service
 echo "Start service"
